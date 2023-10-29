de135f79e05fbf2d3f7fbf1caa4c6b6e95d71a63
tomoya <cj.0s0ttw.nsm4@gmail.com>
2023-10-29 12:02:21 +0900

    CRUDを実装

--- main.go
+++ main.go
@@ -22,24 +22,58 @@ type Todo struct {
 
 func main() {
 	router := gin.Default()
+	USER, PASSWORD, PORT, DBNAME :=  GetEnv()
+	postgresPass := "postgresql://" + USER + ":" + PASSWORD + "@db" + ":" + PORT + "/" + DBNAME
+
+	router.GET("/todos", func(c *gin.Context) {
+		GetTodos(c, postgresPass)
+	})
+
 	router.GET("/todo/:id", func(c *gin.Context) {
-		USER, PASSWORD, PORT, DBNAME :=  GetEnv()
-		postgresPass := "postgresql://" + USER + ":" + PASSWORD + "@db" + ":" + PORT + "/" + DBNAME
 		ShowTodo(c, postgresPass)
 	})
+
+	router.POST("/todo", func(c *gin.Context) {
+		CreateTodo(c, postgresPass)
+	})
+
+	router.PATCH("/todo/:id", func(c *gin.Context) {
+		UpdateTodo(c, postgresPass)
+	})
+
+	router.DELETE("/todo/:id", func(c *gin.Context) {
+		DeleteTodo(c, postgresPass)
+	})
+
 	router.Run(":8080")
 }
 
-func ShowTodo(c *gin.Context, postgresPass string) {
-	id := c.Param("id")
-	
+func GetTodos(c *gin.Context, postgresPass string) {
 	db, err := gorm.Open(postgres.Open(postgresPass), &gorm.Config{})
 	if err != nil {
 		errorMessage := err.Error()
 		fmt.Println("DB接続エラー")
 		panic(errorMessage)
 	}
+	fmt.Println("DB接続に成功しました。")
 
+	var todos []Todo
+  db.Find(&todos)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"todos": todos,
+	})
+}
+
+func ShowTodo(c *gin.Context, postgresPass string) {
+	id := c.Param("id")
+	db, err := gorm.Open(postgres.Open(postgresPass), &gorm.Config{})
+	if err != nil {
+		errorMessage := err.Error()
+		fmt.Println("DB接続エラー")
+		panic(errorMessage)
+	}
 	fmt.Println("DB接続に成功しました。")
 
 	var todo Todo
@@ -50,6 +84,66 @@ func ShowTodo(c *gin.Context, postgresPass string) {
 		"todo": todo,
 	})
 }
+
+func CreateTodo(c *gin.Context, postgresPass string) {
+	db, err := gorm.Open(postgres.Open(postgresPass), &gorm.Config{})
+	if err != nil {
+		errorMessage := err.Error()
+		fmt.Println("DB接続エラー")
+		panic(errorMessage)
+	}
+	fmt.Println("DB接続に成功しました。")
+
+	var newTodo Todo
+	c.BindJSON(&newTodo);
+  db.Create(&newTodo)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"todo": newTodo,
+	})
+}
+
+func UpdateTodo(c *gin.Context, postgresPass string) {
+	id := c.Param("id")
+	db, err := gorm.Open(postgres.Open(postgresPass), &gorm.Config{})
+	if err != nil {
+		errorMessage := err.Error()
+		fmt.Println("DB接続エラー")
+		panic(errorMessage)
+	}
+	fmt.Println("DB接続に成功しました。")
+
+	var updateTodo Todo
+  db.First(&updateTodo, id)
+	c.BindJSON(&updateTodo);
+	db.Save(&updateTodo)
+	
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"todo": updateTodo,
+	})
+} 
+
+func DeleteTodo(c *gin.Context, postgresPass string) {
+	id := c.Param("id")
+	db, err := gorm.Open(postgres.Open(postgresPass), &gorm.Config{})
+	if err != nil {
+		errorMessage := err.Error()
+		fmt.Println("DB接続エラー")
+		panic(errorMessage)
+	}
+	fmt.Println("DB接続に成功しました。")
+
+	var deleteTodo Todo
+  db.Delete(&deleteTodo, id)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+		"todo": deleteTodo,
+	})
+}
+
 
 func GetEnv() (string, string, string, string) {
 	err := godotenv.Load("env/dev.env")
